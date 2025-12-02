11da67dd9cdd5912df73d1a34fbbfe6378c0540d
diepanhnguyen2604-arch <diepanhnguyen2604@gmail.com>
2025-12-03 00:24:03 +0700

    Update iot_static.py

--- topology/iot_static.py
+++ topology/iot_static.py
@@ -1,124 +1,144 @@
-from mininet.net import Mininet
-from mininet.node import Node, OVSController
-from mininet.link import TCLink
+import os
+import time
 from mininet.topo import Topo
+from mininet.node import Node, OVSController
+from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-import os
 
-# =======================================
-# Define a Linux Router Node
-# =======================================
 class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
-        self.cmd('sysctl -w net.ipv4.ip_forward=1')
+        self.cmd("sysctl -w net.ipv4.ip_forward=1")
 
     def terminate(self):
-        self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        self.cmd("sysctl -w net.ipv4.ip_forward=0")
         super(LinuxRouter, self).terminate()
 
-# =======================================
-# Define the Topology
-# =======================================
-class StaticRoutingTopo(Topo):
+class IoTStaticTopo(Topo):
     def build(self):
+        # Routers
+        g1 = self.addNode("g1", cls=LinuxRouter, ip=None)
+        g2 = self.addNode("g2", cls=LinuxRouter, ip=None)
+        g3 = self.addNode("g3", cls=LinuxRouter, ip=None)
 
-        # Routers and gateway
-        gw = self.addNode('gw', cls=LinuxRouter, ip='10.0.10.1/24')
-        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.10.2/24')
-        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.0.20.2/24')
-        r3 = self.addNode('r3', cls=LinuxRouter, ip='10.0.30.2/24')
-
-        # Cloud host
-        cloud = self.addHost('cloud', ip='10.0.100.2/24', defaultRoute='via 10.0.100.1')
+        # Cloud
+        cloud = self.addHost("cloud", ip="10.0.100.2/24", defaultRoute="via 10.0.100.1")
 
         # Switches
-        s1, s2, s3, s4 = [self.addSwitch(s) for s in ('s1', 's2', 's3', 's4')]
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+        s4 = self.addSwitch("s4")
 
-        # Hosts
-        hosts_s1 = [self.addHost(f'h{i}', ip=f'10.0.1.{i}/24', defaultRoute='via 10.0.1.254') for i in range(1, 4)]
-        hosts_s2 = [self.addHost(f'h{i}', ip=f'10.0.2.{i}/24', defaultRoute='via 10.0.2.1') for i in range(4, 6)]
-        hosts_s3 = [self.addHost(f'h{i}', ip=f'10.0.3.{i}/24', defaultRoute='via 10.0.3.1') for i in range(6, 8)]
-        hosts_s4 = [self.addHost(f'h{i}', ip=f'10.0.4.{i}/24', defaultRoute='via 10.0.4.1') for i in range(8, 11)]
+        bw_router = 10
+        bw_host = 10
 
-        # Gateway <-> Routers
-        self.addLink(gw, r1, intfName1='gw-eth1', params1={'ip': '10.0.10.1/24'}) 
-        self.addLink(gw, r2, intfName1='gw-eth2', params1={'ip': '10.0.20.1/24'}) 
-        self.addLink(gw, r3, intfName1='gw-eth3', params1={'ip': '10.0.30.1/24'}) 
+        # --- LINKS ---
+        # G1 <-> Cloud
+        self.addLink(g1, cloud, intfName1="g1-eth0", params1={"ip": "10.0.100.1/24"}, bw=1, max_queue_size=100)
+
+        # G1 <-> S1 
+        self.addLink(g1, s1, intfName1="g1-eth1", params1={"ip": "10.0.1.254/24"}, bw=bw_router)
+
+        # G1 <-> S2 
+        self.addLink(g1, s2, intfName1="g1-eth2", params1={"ip": "10.0.2.254/24"}, bw=bw_router)
+
+        # G1 <-> G2 
+        self.addLink(g1, g2, 
+                     intfName1="g1-eth3", params1={"ip": "10.0.10.1/24"},
+                     intfName2="g2-eth0", params2={"ip": "10.0.10.2/24"}, 
+                     bw=50)
+
+        # G1 <-> G3 
+        self.addLink(g1, g3, 
+                     intfName1="g1-eth4", params1={"ip": "10.0.20.1/24"},
+                     intfName2="g3-eth0", params2={"ip": "10.0.20.2/24"}, 
+                     bw=50)
+
+        # G2 <-> S3 
+        self.addLink(g2, s3, intfName1="g2-eth1", params1={"ip": "10.0.3.254/24"}, bw=bw_router)
+
+        # G3 <-> S4 
+        self.addLink(g3, s4, intfName1="g3-eth1", params1={"ip": "10.0.4.254/24"}, bw=bw_router)
+
+        # --- HOSTS ---
+        # S1 Hosts
+        for i in range(1, 4): 
+            self.addHost(f"h{i}", ip=f"10.0.1.{i}/24", defaultRoute="via 10.0.1.254")
+            self.addLink(s1, f"h{i}", bw=bw_host)
         
-        # Routers <-> Switches
-        self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip': '10.0.1.254/24'})
-        self.addLink(r2, s2, intfName1='r2-eth1', params1={'ip': '10.0.2.1/24'})
-        self.addLink(r2, s3, intfName1='r2-eth2', params1={'ip': '10.0.3.1/24'})
-        self.addLink(r3, s4, intfName1='r3-eth1', params1={'ip': '10.0.4.1/24'})
+        # S2 Hosts
+        for i in range(4, 6): 
+            self.addHost(f"h{i}", ip=f"10.0.2.{i}/24", defaultRoute="via 10.0.2.254")
+            self.addLink(s2, f"h{i}", bw=bw_host)
 
-        # Gateway <-> Cloud
-        self.addLink(gw, cloud, intfName1='gw-eth4', params1={'ip': '10.0.100.1/24'})
+        # S3 Hosts (Gateway G2 .254)
+        for i in range(6, 8): 
+            self.addHost(f"h{i}", ip=f"10.0.3.{i}/24", defaultRoute="via 10.0.3.254")
+            self.addLink(s3, f"h{i}", bw=bw_host)
 
-        # Switch <-> Hosts
-        for h in hosts_s1: self.addLink(s1, h)
-        for h in hosts_s2: self.addLink(s2, h)
-        for h in hosts_s3: self.addLink(s3, h)
-        for h in hosts_s4: self.addLink(s4, h)
+        # S4 Hosts (Gateway G3 .254)
+        for i in range(8, 11): 
+            self.addHost(f"h{i}", ip=f"10.0.4.{i}/24", defaultRoute="via 10.0.4.254")
+            self.addLink(s4, f"h{i}", bw=bw_host)
 
-
-# =======================================
-# Configure Static Routes
-# =======================================
 def run():
-    topo = StaticRoutingTopo()
+    topo = IoTStaticTopo()
     net = Mininet(topo=topo, controller=OVSController, link=TCLink)
     net.start()
 
-    gw, r1, r2, r3 = net.get('gw', 'r1', 'r2', 'r3')
+    g1, g2, g3 = net.get("g1", "g2", "g3")
+    cloud = net.get("cloud")
 
-    info("\n*** Configuring static routes...\n")
+    for r in [g1, g2, g3]:
+        r.cmd("sysctl -w net.ipv4.ip_forward=1")
 
-    # ----------------------------
-    # Gateway
-    # ----------------------------
-    gw.cmd('ip route add 10.0.1.0/24 via 10.0.10.2')
-    gw.cmd('ip route add 10.0.2.0/24 via 10.0.20.2')
-    gw.cmd('ip route add 10.0.3.0/24 via 10.0.20.2')
-    gw.cmd('ip route add 10.0.4.0/24 via 10.0.30.2')
-    gw.cmd('ip route add default via 10.0.100.2')
+    info("\n=== CONFIGURING STATIC ROUTES ===\n")
+    
+    g1.cmd("ip route add 10.0.3.0/24 via 10.0.10.2") 
+    g1.cmd("ip route add 10.0.4.0/24 via 10.0.20.2") 
 
-    # ----------------------------
-    # Router 1 (connects subnet 10.0.1.0/24)
-    # ----------------------------
-    r1.cmd('ip route add default via 10.0.10.1')  # to GW
-    r1.cmd('ip route add 10.0.2.0/24 via 10.0.10.1')
-    r1.cmd('ip route add 10.0.3.0/24 via 10.0.10.1')
-    r1.cmd('ip route add 10.0.4.0/24 via 10.0.10.1')
-    r1.cmd('ip route add 10.0.100.0/24 via 10.0.10.1')
+    g2.cmd("ip route add default via 10.0.10.1")
 
-    # ----------------------------
-    # Router 2 (connects subnets 10.0.2.0 and 10.0.3.0)
-    # ----------------------------
-    r2.cmd('ip route add default via 10.0.20.1')  # to GW
-    r2.cmd('ip route add 10.0.0.0/24 via 10.0.20.1')
-    r2.cmd('ip route add 10.0.4.0/24 via 10.0.20.1')
-    r2.cmd('ip route add 10.0.100.0/24 via 10.0.20.1')
+    g3.cmd("ip route add default via 10.0.20.1")
 
-    # ----------------------------
-    # Router 3 (connects subnet 10.0.4.0/24)
-    # ----------------------------
-    r3.cmd('ip route add default via 10.0.30.1')  # to GW
-    r3.cmd('ip route add 10.0.0.0/24 via 10.0.30.1')
-    r3.cmd('ip route add 10.0.2.0/24 via 10.0.30.1')
-    r3.cmd('ip route add 10.0.3.0/24 via 10.0.30.1')
-    r3.cmd('ip route add 10.0.100.0/24 via 10.0.30.1')  
+    cloud.cmd("ip route add 10.0.0.0/16 via 10.0.100.1")
 
-    info("\n*** All routes configured successfully!\n")
+    info("Routing OK. Waiting 2s...\n")
+    time.sleep(2)
+
+    info("\n=== STARTING SIMULATION ===\n")
+    cloud.cmd("PYTHONIOENCODING=utf-8 python3 iot_server.py > server.log 2>&1 &")
+    cloud.cmd("iperf -s -u -p 5001 &") 
+    time.sleep(2) 
+
+    sensors = {
+        'h1': 'temp', 'h2': 'humid', 'h3': 'motion', 
+        'h4': 'temp', 'h5': 'motion',                
+        'h6': 'humid', 'h7': 'temp',                 
+        'h8': 'motion', 'h9': 'temp', 'h10': 'humid' 
+    }
+    info("[*] Starting Sensors...\n")
+    for hostname, stype in sensors.items():
+        h = net.get(hostname)
+        h.cmd(f"python3 iot_sensor.py {hostname} {stype} &")
+        info(f" -> {hostname} started ({stype})\n")
+
+    info("\n------------------------------------------------\n")
+    info("SDN Simulation Running!\n")
+    info("View Dashboard: http://10.0.100.2\n")
+    info("------------------------------------------------\n")
+
     CLI(net)
+    
+    os.system("pkill -f iot_server.py")
+    os.system("pkill -f iot_sensor.py")
+    os.system("pkill -f iperf")
     net.stop()
 
-
-# =======================================
-# Main
-# =======================================
-if __name__ == '__main__':
-    setLogLevel('info')
-    os.system('sudo mn -c')
+if __name__ == "__main__":
+    setLogLevel("info")
+    os.system("mn -c") 
     run()
